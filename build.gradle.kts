eb8fc1b6a70b8caf630b6766edf83ae819405dfe
Alexey Lapin <alapin@luxoft.com>
2020-04-23 19:08:57 +0300

    improve build script, apply spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,22 @@
 plugins {
     java
     id("com.github.ben-manes.versions")
+    id("com.diffplug.gradle.spotless")
 }
 
+val enableJacoco = project.hasProperty("enableJacoco")
+val jacocoClassesDir = file("$buildDir/jacoco/classes")
+
 allprojects {
-    apply(plugin="idea")
+    apply(plugin = "idea")
+    apply(plugin = "com.diffplug.gradle.spotless")
+
+    if (enableJacoco) {
+        apply(plugin = "jacoco")
+        configure<JacocoPluginExtension> {
+            toolVersion = Versions.jacoco
+        }
+    }
 
     repositories {
         mavenCentral()
@@ -12,7 +24,35 @@ allprojects {
 }
 
 subprojects {
-    apply(plugin="java")
+    apply(plugin = "java")
+
+    pluginManager.withPlugin("java") {
+
+        spotless {
+            val headerFile = rootProject.file("src/spotless/mit-license.java")
+
+            java {
+                licenseHeaderFile(headerFile, "(package|import|open|module) ")
+                removeUnusedImports()
+                trimTrailingWhitespace()
+                endWithNewline()
+            }
+        }
+
+        afterEvaluate {
+            if (enableJacoco) {
+                val jarTask = tasks["jar"] as Jar
+                val extractJar by tasks.registering(Copy::class) {
+                    from(zipTree(jarTask.archiveFile))
+                    into(jacocoClassesDir)
+                    include("**/*.class")
+                    includeEmptyDirs = false
+                    onlyIf { jarTask.enabled }
+                }
+                jarTask.finalizedBy(extractJar)
+            }
+        }
+    }
 
     configure<JavaPluginConvention> {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -22,6 +62,38 @@ subprojects {
     tasks {
         withType<Test> {
             useJUnitPlatform()
+            testLogging {
+                showStackTraces = true
+                exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+            }
+        }
+
+        named("build").get().shouldRunAfter("spotlessJava")
+    }
+}
+
+if (enableJacoco) {
+    tasks {
+        val jacocoMerge by registering(JacocoMerge::class) {
+            subprojects.forEach { project ->
+                executionData(fileTree("dir" to "${project.buildDir}/jacoco", "include" to "*.exec"))
+                dependsOn(project.tasks.withType<Test>())
+            }
+        }
+        register<JacocoReport>("jacocoRootReport") {
+            dependsOn(jacocoMerge)
+            subprojects.forEach { project ->
+                project.pluginManager.withPlugin("java") {
+                    sourceDirectories.from(project.the<SourceSetContainer>()["main"].allSource.srcDirs)
+                }
+            }
+            classDirectories.from(files(jacocoClassesDir))
+            executionData(jacocoMerge.get().destinationFile)
+            reports {
+                html.isEnabled = true
+                xml.isEnabled = true
+                csv.isEnabled = false
+            }
         }
     }
 }
--- src/spotless/mit-license.java
+++ src/spotless/mit-license.java
@@ -21,22 +21,3 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package com.github.al.realworld.application.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class InvalidRequestException extends RuntimeException {
-
-    public InvalidRequestException() {
-    }
-
-    public InvalidRequestException(String message) {
-        super(message);
-    }
-
-    public static InvalidRequestException invalidRequest(String message, Object... args) {
-        return new InvalidRequestException(String.format(message, args));
-    }
-}
