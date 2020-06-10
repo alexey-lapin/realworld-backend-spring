86a7c9aa9529d5a8686853292e62046c4a29048e
Alexey Lapin <alapin@luxoft.com>
2020-06-10 19:11:59 +0300

    bump versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,11 +1,11 @@
 object Versions {
 
     val jwt = "0.11.1"
-    val liquibase = "3.8.7"
+    val liquibase = "3.9.0"
 
     val jacoco = "0.8.5"
 
-    val springFeign = "2.2.2.RELEASE"
-    val feign = "10.7.4"
+    val springFeign = "2.2.3.RELEASE"
+    val feign = "11.0"
 
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 pluginManagement {
     plugins {
         id("com.github.ben-manes.versions") version "0.28.0"
-        id("com.diffplug.gradle.spotless") version "3.28.1"
-        id("org.springframework.boot") version "2.2.6.RELEASE"
+        id("com.diffplug.gradle.spotless") version "4.3.0"
+        id("org.springframework.boot") version "2.3.0.RELEASE"
         id("io.spring.dependency-management") version "1.0.9.RELEASE"
     }
 }
