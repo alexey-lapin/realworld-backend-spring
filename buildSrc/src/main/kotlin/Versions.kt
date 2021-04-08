091eec854bd64bb4dd0900cce83ab488deb7900d
Alexey Lapin <alexey-lapin@protonmail.com>
2021-04-08 20:31:37 +0300

    bump versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,11 +1,11 @@
 object Versions {
 
     val jwt = "0.11.2"
-    val liquibase = "4.3.1"
+    val liquibase = "4.3.2"
 
     val jacoco = "0.8.5"
 
-    val springFeign = "3.0.1"
+    val springFeign = "3.0.2"
     val feign = "11.0"
 
 }
