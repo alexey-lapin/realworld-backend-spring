8299afb04f1ae9fdf2e4bcbd3370827b52c8e661
Alexey Lapin <alexey-lapin@protonmail.com>
2021-04-29 22:17:58 +0300

    bump versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
     plugins {
-        id("com.diffplug.spotless") version "5.12.1"
+        id("com.diffplug.spotless") version "5.12.4"
         id("com.github.ben-manes.versions") version "0.38.0"
         id("org.springframework.boot") version "2.4.5"
         id("io.spring.dependency-management") version "1.0.11.RELEASE"
