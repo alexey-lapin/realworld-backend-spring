0a8d3206b43282ee1593e7326b6d846756b9fea0
Alexey Lapin <alexey-lapin@protonmail.com>
2021-06-20 16:18:50 +0300

    bump versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,11 +1,11 @@
 object Versions {
 
     val jwt = "0.11.2"
-    val liquibase = "4.3.5"
+    val liquibase = "4.4.0"
 
     val jacoco = "0.8.5"
 
-    val springFeign = "3.0.2"
+    val springFeign = "3.0.3"
     val feign = "11.0"
 
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
     plugins {
-        id("com.diffplug.spotless") version "5.13.0"
+        id("com.diffplug.spotless") version "5.14.0"
         id("com.github.ben-manes.versions") version "0.39.0"
         id("org.springframework.boot") version "2.5.1"
         id("io.spring.dependency-management") version "1.0.11.RELEASE"
