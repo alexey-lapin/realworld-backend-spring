517e7eb247e9a7810aafa54d83108d7734efc81a
Alexey Lapin <alexey-lapin@protonmail.com>
2026-01-21 20:13:43 -0500

    Add OpenTelemetry support

---
+++ service/src/main/java/com/github/al/realworld/infrastructure/config/OtelConfig.java
@@ -0,0 +1,55 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 - present Alexey Lapin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.github.al.realworld.infrastructure.config;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class OtelConfig {
+
+    @Bean
+    InstallOpenTelemetryAppender installOpenTelemetryAppender(OpenTelemetry openTelemetry) {
+        return new InstallOpenTelemetryAppender(openTelemetry);
+    }
+
+    static class InstallOpenTelemetryAppender implements InitializingBean {
+
+        private final OpenTelemetry openTelemetry;
+
+        InstallOpenTelemetryAppender(OpenTelemetry openTelemetry) {
+            this.openTelemetry = openTelemetry;
+        }
+
+        @Override
+        public void afterPropertiesSet() {
+            OpenTelemetryAppender.install(this.openTelemetry);
+        }
+    }
+
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,7 @@ dependencyResolutionManagement {
     versionCatalogs {
         generate("libs") {
             fromToml("springBootDependencies")
+            fromToml("opentelemetryInstrumentationDependencies")
         }
     }
 }
