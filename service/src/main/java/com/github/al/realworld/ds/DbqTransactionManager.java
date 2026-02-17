1595b62f1cf75d43d0fac60ebf483c0848e9b02d
Alexey Lapin <alexey-lapin@protonmail.com>
2026-02-16 21:44:52 -0500

    ds

--- service/src/main/java/com/github/al/realworld/ds/DbqTransactionManager.java
+++ service/src/main/java/com/github/al/realworld/ds/DbqTransactionManager.java
@@ -10,77 +10,72 @@ import org.springframework.transaction.TransactionException;
 import javax.sql.DataSource;
 import java.lang.invoke.CallSite;
 import java.lang.invoke.LambdaMetafactory;
-import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 
 public class DbqTransactionManager extends JdbcTransactionManager {
 
-    private static final ConnectionHolderSetter connectionHolderSetter;
+    private static final String TRANSACTION_OBJECT_CLASS_NAME =
+            DataSourceTransactionManager.class.getName() + "$DataSourceTransactionObject";
 
-    static {
-        try {
-            MethodHandles.Lookup myLookup = MethodHandles.lookup();
+    private static final MethodType SET_CONNECTION_HOLDER_METHOD_TYPE =
+            MethodType.methodType(void.class, ConnectionHolder.class, boolean.class);
 
-            // This lookup is only to crack Spring's private nested class / method.
-            MethodHandles.Lookup springLookup =
-                    MethodHandles.privateLookupIn(DataSourceTransactionManager.class, myLookup);
-
-            Class<?> inner = Class.forName(
-                    DataSourceTransactionManager.class.getName() + "$DataSourceTransactionObject"
-            );
-
-            // Direct MH: (inner, ConnectionHolder, boolean)void
-            MethodHandle impl = springLookup.findVirtual(
-                    inner,
-                    "setConnectionHolder",
-                    MethodType.methodType(void.class, ConnectionHolder.class, boolean.class)
-            );
-
-            MethodType samType = MethodType.methodType(
+    private static final MethodType SETTER_FUNCTION_SIGNATURE =
+            MethodType.methodType(
                     void.class,
                     JdbcTransactionObjectSupport.class,
                     ConnectionHolder.class,
                     boolean.class
             );
 
-            MethodType instantiatedType = MethodType.methodType(
-                    void.class,
-                    inner,
-                    ConnectionHolder.class,
-                    boolean.class
-            );
-
-            CallSite cs = LambdaMetafactory.metafactory(
-                    springLookup,
-                    "accept",
-                    MethodType.methodType(ConnectionHolderSetter.class),
-                    samType,
-                    impl,
-                    instantiatedType
-            );
-
-            connectionHolderSetter = (ConnectionHolderSetter) cs.getTarget().invokeExact();
-        } catch (Throwable ex) {
-            throw new RuntimeException(ex);
-        }
-    }
+    private static final ConnectionHolderSetter CONNECTION_HOLDER_SETTER = initializeConnectionHolderSetter();
 
     public DbqTransactionManager(DataSource dataSource) {
         super(dataSource);
     }
 
+    private static ConnectionHolderSetter initializeConnectionHolderSetter() {
+        try {
+            var callerLookup = MethodHandles.lookup();
+            var springLookup = MethodHandles.privateLookupIn(DataSourceTransactionManager.class, callerLookup);
+            var transactionObjectClass = Class.forName(TRANSACTION_OBJECT_CLASS_NAME);
+
+            var setConnectionHolderHandle = springLookup.findVirtual(
+                    transactionObjectClass,
+                    "setConnectionHolder",
+                    SET_CONNECTION_HOLDER_METHOD_TYPE
+            );
+
+            var instantiatedSignature = MethodType.methodType(
+                    void.class,
+                    transactionObjectClass,
+                    ConnectionHolder.class,
+                    boolean.class
+            );
+
+            var callSite = LambdaMetafactory.metafactory(
+                    springLookup,
+                    "accept",
+                    MethodType.methodType(ConnectionHolderSetter.class),
+                    SETTER_FUNCTION_SIGNATURE,
+                    setConnectionHolderHandle,
+                    instantiatedSignature
+            );
+
+            return (ConnectionHolderSetter) callSite.getTarget().invokeExact();
+        } catch (Throwable ex) {
+            throw new IllegalStateException("Failed to initialize transaction connection holder setter", ex);
+        }
+    }
+
     @Override
     protected void doBegin(Object transaction, TransactionDefinition definition) throws TransactionException {
-        if (definition instanceof ConnectionAwareTransactionDefinition def) {
-            if (transaction instanceof JdbcTransactionObjectSupport txObject) {
-                try {
-                    var connection = new CloseSuppressingConnectionWrapper(def.getConnection());
-                    connectionHolderSetter.accept(txObject, new ConnectionHolder(connection), true);
-                } catch (Throwable e) {
-                    throw new RuntimeException(e);
-                }
-            }
+        if (definition instanceof ConnectionAwareTransactionDefinition connectionAwareDefinition
+                && transaction instanceof JdbcTransactionObjectSupport transactionObject) {
+            var connection = new CloseSuppressingConnectionWrapper(connectionAwareDefinition.getConnection());
+            var connectionHolder = new ConnectionHolder(connection);
+            CONNECTION_HOLDER_SETTER.accept(transactionObject, connectionHolder, true);
         }
         super.doBegin(transaction, definition);
     }
